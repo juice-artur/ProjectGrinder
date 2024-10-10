22169d2be6496a464b5becc69fb6295af52a4750
juice-artur <asasin.lol10@gmail.com>
2024-10-10 15:34:38 +0300

    init: create project

---
+++ Source/ProjectGrinder/Hero/PGHero.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "PGHero.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTGRINDER_API APGHero : public APaperZDCharacter
+{
+	GENERATED_BODY()
+	
+};
