a779ec2cb91e163ffb4648ad186dfd68a90469ce
juice-artur <59645676+juice-artur@users.noreply.github.com>
2024-10-12 13:36:52 +0300

    feat: implement character movement (#1)
    * feat: implement move right

--- Source/ProjectGrinder/Hero/PGHero.cpp
+++ Source/ProjectGrinder/Hero/PGHero.cpp
@@ -2,4 +2,57 @@
 
 
 #include "PGHero.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+APGHero::APGHero()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
+	SpringArmComponent->SetupAttachment(GetRootComponent());
+	SpringArmComponent->bUsePawnControlRotation = false;
+	SpringArmComponent->SocketOffset = FVector(0.0f, 100.0f, 80.0f);
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+	CameraComponent->SetupAttachment(SpringArmComponent);
+
+}
+
+void APGHero::OnMoveForward(const FInputActionValue& Value)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Your message"));
+	float Movement = Value.Get<float>();
+	AddMovementInput(FVector(1, 0,0), Movement);
+}
+
+void APGHero::OnJump(const FInputActionValue& Value)
+{
+}
+
+
+void APGHero::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	UEnhancedInputComponent* Input = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	check(Input);
+	Input->BindAction(MoveForwardInputAction, ETriggerEvent::Triggered, this, &APGHero::OnMoveForward);
+}
+
+void APGHero::BeginPlay()
+{
+	if (const APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			constexpr int32 Priority = 0;
+			Subsystem->AddMappingContext(InputMapping, Priority);
+		}
+	}
+
+	Super::BeginPlay();
+}
+
 
--- Source/ProjectGrinder/Hero/PGHero.h
+++ Source/ProjectGrinder/Hero/PGHero.h
@@ -4,7 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "PaperZDCharacter.h"
+#include "InputActionValue.h"
 #include "PGHero.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+class USpringArmComponent;
+class UCameraComponent;
 
 /**
  * 
@@ -13,5 +19,30 @@ UCLASS()
 class PROJECTGRINDER_API APGHero : public APaperZDCharacter
 {
 	GENERATED_BODY()
+public:
+	APGHero();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UCameraComponent* CameraComponent;
+
+
+	UPROPERTY(EditDefaultsOnly, Category = "HeroInput")
+	TObjectPtr<UInputAction> MoveForwardInputAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "HeroInput")
+	TObjectPtr<UInputAction> JumpInputAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "HeroInput")
+	TObjectPtr<UInputMappingContext> InputMapping;
+
+	void OnMoveForward(const FInputActionValue& Value);
+	void OnJump(const FInputActionValue& Value);
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+	virtual void BeginPlay() override;
 	
 };
